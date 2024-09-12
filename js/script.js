2e679cc7924c7ab6ad02fe5d2b1c7312824c48f6
tamagoTMT <karibu34@gmail.com>
2024-09-13 03:27:34 +0900

    Feature/op対応 (#10)
    * サンプル振付師写真を削除
    
    * デフォルト,ゲスト画像追加
    
    * 手直ししました。

--- js/script.js
+++ js/script.js
@@ -101,20 +101,27 @@ document.addEventListener("DOMContentLoaded", function () {
       .join(",");
   };
 
-  const renderNumberContent = (container, number, imgPath, index) => {
-    const choreographerInstagramLinks = renderInstagramLinks(
-      number.instagram || []
-    );
-    const performerIds = formatPerformerIds(number.performers);
-
-    const performersList = number.performers
-      .map((performer) => {
-        const name = getPerformerNameById(performer.id);
-        return `<li><a href="performer.html?id=${performer.id}">${name}</a></li>`;
-      })
-      .join("");
-
-    container.innerHTML = `
+    const renderNumberContent = (container, number, imgPath, index) => {
+        const choreographerInstagramLinks = renderInstagramLinks(number.instagram || []);
+        const performerIds = formatPerformerIds(number.performers);
+    
+        // Performersのリストを生成。配列または文字列に対応。
+        let performersList = '';
+        if (number.performers && Array.isArray(number.performers)) {
+            performersList = number.performers.map(performer => {
+                const name = getPerformerNameById(performer.id);
+                return `<li><a href="performer.html?id=${performer.id}">${name}</a></li>`;
+            }).join('');
+        } else if (typeof number.performers === 'string') {
+            performersList = `<li>${number.performers}</li>`; // 文字列の場合、リンクなしで表示
+        }
+    
+        // Membersセクションのリンクを表示するかチェック
+        const membersSection = performersList
+            ? `<h4><a href="./performer.html?id=${performerIds}">Members</a></h4>`
+            : ''; // 0件の場合はリンクもメンバーリストも表示しない
+    
+        container.innerHTML = `
             <div class="content">
                 <h3 class="sample_text16" style="--number-color: ${
                   number.color
@@ -131,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <p>${number.bioContent}</p>
                 <h4>${number.commentTitle}</h4>
                 <p>${number.comment}</p>
-                <h4><a href="/page/performer.html?id=${performerIds}">Members</a></h4>
+                ${membersSection}
                 <ul>
                     ${performersList}
                 </ul>
